5f68358a7fd6fb21fb0067407e56398f2b8903d8
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-06-30 13:08:08 -0700

    Assignment 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /*
- * Do not use printf, use put main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise 
- */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
