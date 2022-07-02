37fca8258223e36570cdc3e374cf57038f296f7a
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-01 21:10:01 -0700

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (m = 0; m < 10; m++)
+		for (m = 0; m <= 9; m++)
 		{
-			for (l = 0; l < 10; l++)
+			for (l = 0; l <= 9; l++)
 			{
 				putchar(n);
 				putchar(m);
