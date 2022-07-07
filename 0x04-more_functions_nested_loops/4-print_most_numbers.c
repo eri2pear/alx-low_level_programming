f82b41330e58f6645da1778c6858957cce957a1b
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-07 16:16:26 -0700

    7. I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,8 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
- * */
+ */
+
 void print_most_numbers(void)
 {
 	int i;
@@ -12,7 +13,7 @@ void print_most_numbers(void)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 
