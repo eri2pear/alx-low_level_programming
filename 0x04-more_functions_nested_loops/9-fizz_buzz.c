83174dbfaf9216569aebfca5bcc914ca7a086a61
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-07 17:00:31 -0700

    11. The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,31 +3,34 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three, prints Fizz instead of the number
+ * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i <= 100; i++)
+	n = 1;
+	printf(" %d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 !=0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		} else if (n % 5 == 0)
+		{
+			printf(" Buzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
 		} else
 		{
-			printf(" %d", i);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
