8b0b96e865c8d9f603be2efada144316e7414112
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-07 23:20:01 -0700

    Update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -20,16 +20,13 @@ int main(void)
 		max = 2;
 		n /= 2;
 	}
-	
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
 			max = i;
 			n = n / i;
-		}
-	}
-	 (n > 2)
+		if (n > 2)
 		max = n;
 
 	printf("%ld\n", max);
