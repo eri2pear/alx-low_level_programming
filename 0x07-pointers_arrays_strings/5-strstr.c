2b9e327a2607b06ed444e7be16cada444c7a5690
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-18 10:49:47 -0700

    Advanced Tasks

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; j++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
-		{	
+		{
 			if (haystack[i + j] != needle[j])
 				break;
 		}
