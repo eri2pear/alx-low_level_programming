83d05d461145621dea75e3686a1320e6a6cc4f39
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-07-21 08:17:17 -0700

    Correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -83,9 +83,10 @@ int bandersnatch(char *s1, char *s2)
 	 */
 	if (*s1 == '\0')
 		return (0);
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
+	if (*s1 == *s2 && wildcmp(s1, s2) == 1)
+		return (1);
+	else
+	return (bandersnatch((s1 + 1), s2));
 }
 
 /**
